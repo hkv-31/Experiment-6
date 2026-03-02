371a9892ef724ea5e56cf0b5a3cd29fb7bdcff37
HetanshiVora <hetanshi531@gmail.com>
2026-03-02 09:07:11 +0530

    origin

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1>Hetanshi</h1><br><h2>App ID= 2400173</h2>"
-
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
