b278957ac4011fd43c8afd3363454ab1d96aee95
Hetanshi <135648477+hkv-31@users.noreply.github.com>
2026-03-02 07:55:08 +0530

    Edited app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Running on azure"
+def index():
+    return "<h1>Hetanshi</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
